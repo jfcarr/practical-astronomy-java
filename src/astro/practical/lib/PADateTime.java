8611b0cf589d5558649a502aa65b64e4ce57b9ec
Jim Carr <jfcarr@gmail.com>
2021-08-16 22:59:44 -0400

    Explicit types in declarations.

--- src/astro/practical/lib/PADateTime.java
+++ src/astro/practical/lib/PADateTime.java
@@ -12,23 +12,23 @@ public class PADateTime {
 	public CivilDate getDateOfEaster(int inputYear) {
 		double year = inputYear;
 
-		var a = year % 19;
-		var b = Math.floor(year / 100);
-		var c = year % 100;
-		var d = Math.floor(b / 4);
-		var e = b % 4;
-		var f = Math.floor((b + 8) / 25);
-		var g = Math.floor((b - f + 1) / 3);
-		var h = ((19 * a) + b - d - g + 15) % 30;
-		var i = Math.floor(c / 4);
-		var k = c % 4;
-		var l = (32 + 2 * (e + i) - h - k) % 7;
-		var m = Math.floor((a + (11 * h) + (22 * l)) / 451);
-		var n = Math.floor((h + l - (7 * m) + 114) / 31);
-		var p = (h + l - (7 * m) + 114) % 31;
+		double a = year % 19;
+		double b = Math.floor(year / 100);
+		double c = year % 100;
+		double d = Math.floor(b / 4);
+		double e = b % 4;
+		double f = Math.floor((b + 8) / 25);
+		double g = Math.floor((b - f + 1) / 3);
+		double h = ((19 * a) + b - d - g + 15) % 30;
+		double i = Math.floor(c / 4);
+		double k = c % 4;
+		double l = (32 + 2 * (e + i) - h - k) % 7;
+		double m = Math.floor((a + (11 * h) + (22 * l)) / 451);
+		double n = Math.floor((h + l - (7 * m) + 114) / 31);
+		double p = (h + l - (7 * m) + 114) % 31;
 
-		var day = p + 1;
-		var month = n;
+		double day = p + 1;
+		double month = n;
 
 		return new CivilDate((int) month, day, (int) year);
 	}
