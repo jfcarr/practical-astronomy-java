f8a31205274a417e0491a60520323b357ebf238d
Jim Carr <jfcarr@gmail.com>
2021-09-06 15:34:02 -0400

    Convert -> Equatorial Coordinates <-> Galactic Coordinates

--- src/astro/practical/containers/GalacticCoordinates.java
+++ src/astro/practical/containers/GalacticCoordinates.java
@@ -2,8 +2,8 @@ package astro.practical.containers;
 
 import astro.practical.types.CoordinatesLongLat;
 
-public class EclipticCoordinates extends CoordinatesLongLat {
-	public EclipticCoordinates(double longitudeDegrees, double longitudeMinutes, double longitudeSeconds,
+public class GalacticCoordinates extends CoordinatesLongLat {
+	public GalacticCoordinates(double longitudeDegrees, double longitudeMinutes, double longitudeSeconds,
 			double latitudeDegrees, double latitudeMinutes, double latitudeSeconds) {
 		super(longitudeDegrees, longitudeMinutes, longitudeSeconds, latitudeDegrees, latitudeMinutes, latitudeSeconds);
 	}
