8dafd7bdb1f79cbd962f33da1dfcbf280bb39511
Jim Carr <jim.carr@devtoprd.com>
2024-09-24 19:32:32 -0400

    Updated documentation

--- docs/classastro_1_1practical_1_1lib_1_1PASun.js
+++ docs/classastro_1_1practical_1_1lib_1_1PASun.js
@@ -4,6 +4,7 @@ var classastro_1_1practical_1_1lib_1_1PASun =
     [ "equationOfTime", "classastro_1_1practical_1_1lib_1_1PASun.html#ac45d47b68addb88e7eb1fb1170803024", null ],
     [ "morningAndEveningTwilight", "classastro_1_1practical_1_1lib_1_1PASun.html#a9a0bab5e1ca305f492bddd38947ba04c", null ],
     [ "precisePositionOfSun", "classastro_1_1practical_1_1lib_1_1PASun.html#a349bb3a0a5d533a555b490733aa804e9", null ],
+    [ "solarElongation", "classastro_1_1practical_1_1lib_1_1PASun.html#a45d600ee376cc8ab7150aee19b923405", null ],
     [ "sunDistanceAndAngularSize", "classastro_1_1practical_1_1lib_1_1PASun.html#a61435893e71799562fa36c372f56bb8c", null ],
     [ "sunriseAndSunset", "classastro_1_1practical_1_1lib_1_1PASun.html#a22f8c8d278d1a7ce3d099251668447f4", null ]
 ];
